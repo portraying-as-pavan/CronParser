8300b3d87c8e5899e1d09a37eb187f4b82e6777b
portraying-as-pavan <pavanguguloth15@gmail.com>
2024-02-22 19:21:06 +0530

    remove unneccesary code

--- src/main/java/com/deliveroo/services/impl/MinutesFieldParserImpl.java
+++ src/main/java/com/deliveroo/services/impl/MinutesFieldParserImpl.java
@@ -12,18 +12,10 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class MinutesFieldParserImpl implements MinuteFieldParser {
-
-        private final String REGEX=
-                "^(\\\\*|[0-5]?[0-9]|\\\\*/\\\\d+|[0-5]?[0-9]-[0-5]?[0-9]|([0-5]?[0-9],)+[0-5]?[0-9])$\n";
-
-        private final int MIN =0, MAX =59;
+    private final int MIN =0, MAX =59;
     @Override
     public List<String> parseField(String field) throws InvalidFieldException {
-        Pattern pattern=Pattern.compile(REGEX);
-        Matcher matcher=pattern.matcher(field);
-
 
         return CronParserUtil.parseNumericalField(field,MIN,MAX);
-
     }
 }
